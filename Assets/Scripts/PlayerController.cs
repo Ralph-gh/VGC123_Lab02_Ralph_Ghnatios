8384031cccd180e141889da0614c41717501c782
Ralph-gh <ralphghnatios@gmail.com>
2024-10-21 12:05:37 -0400

    collisions are complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Bindings;
+using UnityEngine.Internal;
+
+
+
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [Range(3,10)]
+    [Range(3, 10)]
     public float speed = 5.5f;
     Rigidbody2D rb;
-    // Start is called before the first frame update
 
+
+    //awake happens before everything
     private void Awake()
     {
-        
+
     }
+    // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horinzontal");
+        float hInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+        float vInput = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x,vInput * speed);
     }
 }
